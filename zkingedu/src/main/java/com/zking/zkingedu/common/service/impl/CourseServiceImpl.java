1f0e864efa9a74a2a3cfba95f19b4d0ac02f3970
liuxuqing <longhuibiao@qq.com>
2019-06-24 13:47:21 +0800

    加了个CourseController

--- zkingedu/src/main/java/com/zking/zkingedu/common/service/impl/CourseServiceImpl.java
+++ zkingedu/src/main/java/com/zking/zkingedu/common/service/impl/CourseServiceImpl.java
@@ -1,7 +1,12 @@
 package com.zking.zkingedu.common.service.impl;
 
+import com.zking.zkingedu.common.dao.CourseDao;
+import com.zking.zkingedu.common.model.Course;
 import com.zking.zkingedu.common.service.CourseService;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 课程接口
@@ -10,4 +15,17 @@ import org.springframework.stereotype.Service;
  */
 @Service("courseService")
 public class CourseServiceImpl implements CourseService {
+    @Resource
+    private CourseDao courseDao;
+
+    /**
+     * 根据类别和课程体系查询课程
+     * @param free
+     * @param fid
+     * @return
+     */
+    @Override
+    public List<Course> courses(Integer free, Integer fid) {
+        return courseDao.courses(free,fid);
+    }
 }
--- zkingedu/src/main/java/com/zking/zkingedu/common/service/impl/SystemServiceImpl.java
+++ zkingedu/src/main/java/com/zking/zkingedu/common/service/impl/SystemServiceImpl.java
@@ -1,7 +1,12 @@
 package com.zking.zkingedu.common.service.impl;
 
+import com.zking.zkingedu.common.dao.SystemDao;
+import com.zking.zkingedu.common.model.System;
 import com.zking.zkingedu.common.service.SystemService;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 课程体系接口   服务实现层
@@ -9,4 +14,15 @@ import org.springframework.stereotype.Service;
  */
 @Service("systemService")
 public class SystemServiceImpl implements SystemService {
+    @Resource
+    private SystemDao systemDao;
+
+    /**
+     * 获取所有体系
+     * @return
+     */
+    @Override
+    public List<System> systems() {
+        return systemDao.systems();
+    }
 }
