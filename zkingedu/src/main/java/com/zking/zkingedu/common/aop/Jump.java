cca5ee02782701af56716c80b2ddd8a7bceed41a
longaipiao <longhuibiao@qq.com>
2019-06-25 18:40:59 +0800

    蒋奇提交更改

--- zkingedu/src/main/java/com/zking/zkingedu/common/aop/Jump.java
+++ zkingedu/src/main/java/com/zking/zkingedu/common/aop/Jump.java
@@ -3,10 +3,13 @@ package com.zking.zkingedu.common.aop;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class Jump {
     @RequestMapping(value = "/jump")
-    public String jump(){
+    public String jump(HttpServletRequest request){
+        request.setAttribute("addr","delete");
         System.out.println("jump");
         return "user/index";
     }
